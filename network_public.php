35ef3b67760033ddcef58f754afaba3a4acae47f
KJ <kmj442@gmail.com>
2018-11-23 23:04:49 -0500

    Add files via upload
    created by: https://www.reddit.com/user/radcliff

---
+++ network_public.php
@@ -0,0 +1,31 @@
+// Contributed by Reddit User: /u/radcliff
+//https://www.reddit.com/user/radcliff
+
+<?php
+
+error_reporting(0);
+
+
+$ip = `curl -s ifconfig.me`;
+
+$ip = trim($ip);
+
+$output = `networksetup -getairportnetwork en0`;
+
+$wifi = 'No wifi';
+if (preg_match("/Current\sWi\-Fi\sNetwork\:\s(.*?)$/", $output, $matches) == true)
+{
+	$wifi = trim($matches[1]);
+
+}
+
+$output = `ifconfig en0 inet`;
+
+$local = 'No local IP';
+if (preg_match("/.*?inet\s(.*?)\snetmask.*?/", $output, $matches) == true)
+{
+	$local = trim($matches[1]);
+
+}
+
+echo "Pub: $ip\nWiFi: $wifi\nen0: $local";
