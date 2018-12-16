76b38dce634b20a3eb1ea1b013639aa83b4ebf84
KevinJohnson <kmj442@gmail.com>
2018-12-15 22:53:18 -0500

    Reads config file now added base config file

--- plex_check_public.py
+++ plex_check_public.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python2
 from requests import ConnectionError
 from plexapi.server import PlexServer
+import yaml
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--config_loc",
+                    help="Location file - neccessary")
+
+args = parser.parse_args()
 
 class Plex_Handler():
 
-    def __init__(self):
-        token = "<input_token_here"
-        baseurl = "<input_ip_address_here>:32400"
+    def __init__(self, config):
+        """ __init__ """
+        token = config["Plex"]["token"]
+        baseurl = "http://{0}:32400".format(config["Plex"]["ip"])
         try:
             self.plex = PlexServer(baseurl, token)
             movie_continue = True
@@ -17,16 +26,26 @@ class Plex_Handler():
             self.get_avail_movies()
 
     def get_avail_movies(self):
+        """ Queries movie list to see if it retrieves index """
         movies = self.plex.library.section("Movies")
         avail_movies = len(movies.search())
         if avail_movies > 0:
             print("Up")
+            return "Up"
         else:
             print("Down")
+            return "Down"
 
+def get_config():
+    """ Reads Config file to pass in """
+    file_stream = open(args.config_loc, "r")
+    config = yaml.load(file_stream)
+    file_stream.close()
+    return config
 
 def main():
-    server = Plex_Handler()
+    config = get_config()
+    server = Plex_Handler(config)
 
 if __name__ == '__main__':
     main()
